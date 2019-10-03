751891f4c5bfb6f92e8c0c7492924339155c1eaa
Just-a-programmer <53569587+Just-a-programmer@users.noreply.github.com>
2019-10-03 18:53:13 +0530

    Add files via upload

--- Assembler/src/Main.java
+++ Assembler/src/Main.java
@@ -4,23 +4,59 @@ import java.util.ArrayList;
 
 public class Main {
 
+    private static ArrayList<Symbol> symbolTable=new ArrayList<Symbol>();
+    private static ArrayList<Opcode> opcodeTable=new ArrayList<Opcode>();
     public static void main(String[] args) throws IOException {
 	// write your code here
-        ArrayList<Symbol> symbolTable;
-        ArrayList<Opcode> opcodeTable;
-        BufferedReader input = new BufferedReader(new FileReader("sample_input.txt"));
+
+        BufferedReader input = new BufferedReader(new FileReader("./Assembler/sample_input.txt"));
         passOne(input);
     }
+    public static boolean is_not_comment(String line){
+        int flag=0;
+        for(int i=0;i<line.length();i++)
+        {
+            if(Character.isLetter(line.charAt(i)))
+                return true;
+            else if(line.charAt(i)=='/'&&flag==0)
+                flag=1;
+            else if(line.charAt(i)=='/'&&flag==1)
+                return false;
+        }
+        return false;
+    }
+    public static String check_label(String line){
+        int flag=0;
+        String ans="";
+        for(int i=0;i<line.length();i++)
+        {
 
+            if(line.charAt(i)==':')
+                return ans;
+            else if(!(line.charAt(i)==' ')&&flag==0)
+                ans+=String.valueOf(line.charAt(i));
+            else if(flag==0&&line.charAt(i)==' '&&ans.length()==0)
+                flag=1;
+            else if(!(line.charAt(i)==' ')&&flag==1)
+                return null;
+
+        }
+        return null;
+    }
     public static void passOne(BufferedReader input) throws IOException {
 
-        String line, symbol, opcode;
+        String line, label, opcode;
         int locationCounter, length, value, type;
         locationCounter = 0;
+        String moreInput;
+        while ((moreInput = input.readLine()) != null) {
+            if(is_not_comment(moreInput))
+            {
+                label=check_label(moreInput);
+                if(label!=null)
+                    symbolTable.add(new Symbol(label,"Label",locationCounter,-1,12));
+            }
 
-        int moreInput;
-        while ((moreInput = input.read()) != -1) {
-            System.out.println((char) moreInput);
         }
 
         input.close();
