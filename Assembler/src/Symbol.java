f2114e2d38abffbf5243a57528a671ef6ea8327a
Just-a-programmer <53569587+Just-a-programmer@users.noreply.github.com>
2019-10-04 17:02:19 +0530

    Update Symbol.java

--- Assembler/src/Symbol.java
+++ Assembler/src/Symbol.java
@@ -1,15 +1,31 @@
 public final class Symbol {
     public final String name;
-    public final String type;
-    public final int offset;
+    public String type;
+    public  int offset;
     public final int value;
     public final int size;
-
+    @Override
+    public String toString()
+    {
+        String x="";
+        x+=name+" "+type+" "+String.valueOf(offset)+" "+String.valueOf(value)+" "+String.valueOf(size);
+        return x;
+    }
+    public void update(Symbol o)
+    {
+        if(o.offset!=-1)
+            offset=o.offset;
+        type=o.type;
+    }
     public Symbol(String name, String type, int offset, int value, int size) {
         this.name = name;
         this.type = type;
         this.offset = offset;
         this.value = value;
         this.size = size;
+    }
+    public String getName()
+    {
+        return name;
     }
 }
